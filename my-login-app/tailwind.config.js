042d184bbf486c2fac4f4582c09509c9b73254e6
SAGAR KUMAR <sagarazm999@gmail.com>
2024-09-28 09:45:08 +0530

    login page

---
+++ my-login-app/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        peach: {
+          100: '#FFEDE5', // Adjust to your preferred shade
+        },
+      },
+    },
+  },
+  plugins: [],
+}
